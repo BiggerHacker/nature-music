2ebdedbf6d2ef628737b12215b5227bda053f412
biggerHacker <1049025971@qq.com>
2017-06-28 23:18:25 +0800

    fix: v-album btns cursor

--- src/components/v-album.vue
+++ src/components/v-album.vue
@@ -41,6 +41,7 @@
       border: 1px solid $select-bg-color;
       color: $select-bg-color;
       &.active {
+        cursor: default;
         border: 1px solid #e9e9e9;
         color: #e9e9e9;
       }
