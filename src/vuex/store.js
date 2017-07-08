879aefd7544f4662ed77f97e99bc433714366957
biggerHacker <1049025971@qq.com>
2017-07-08 17:40:25 +0800

    store.js

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,10 +5,9 @@ Vue.use(Vuex)
 
 import setSheetID from './modules/setSheetID'
 
-let debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   modules: {
     setSheetID
   },
-  strict: debug
+  strict: process.env.NODE_ENV !== 'production'
 })
