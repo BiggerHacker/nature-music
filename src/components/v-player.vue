28884737bcd52fecc3da00cf732aa6cd13293d42
biggerHacker <1049025971@qq.com>
2017-07-11 20:43:43 +0800

    doc: player

---
+++ src/components/v-player.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="player">
+    player
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'player'
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
