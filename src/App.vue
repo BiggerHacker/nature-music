b22431b9a77fe907652dac7fd170d63f094471ea
biggerHacker <1049025971@qq.com>
2017-07-12 20:37:08 +0800

    doc: player

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,27 @@
   <div class="music-wrapper">
     <v-menu></v-menu>
     <v-section></v-section>
+    <v-player></v-player>
   </div>
 </template>
 
 <script>
   import VMenu from '@/components/v-menu'
   import VSection from '@/components/v-section'
+  import VPlayer from '@/components/v-player'
   export default {
     name: 'app',
     components: {
       VMenu,
-      VSection
+      VSection,
+      VPlayer
     }
   }
 </script>
 
 <style lang="scss" scoped>
   .music-wrapper {
+    position: relative;
     width: 1200px;
     height: 100%;
     margin: 0 auto;
--- src/components/v-player.vue
+++ src/components/v-player.vue
@@ -1,15 +1,61 @@
 <template>
   <div class="player">
-    player
+    <div class="mini-player">mini</div>
+    <div class="spread-player" :style="{height: spreadHeight}">spread</div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'player'
+    name: 'player',
+    data () {
+      return {
+        spreadHeight: 0
+      }
+    },
+    created () {
+      this.spreadHeight = document.body.clientHeight + 'px'
+      window.addEventListener('resize', () => {
+        this.spreadHeight = document.body.clientHeight + 'px'
+      })
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  
+  @import '~@/assets/scss/variable';
+  .player {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+  }
+  .mini-player {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    border-top: 1px solid $border-color;
+    background-color: $select-bg-color;
+  }
+  .spread-player {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 8;
+    width: 1200px;
+    margin: 0 auto;
+    transition: all .18s ease-out;
+    transform: translateY(100%);
+    background-color: $black;
+    color: $white;
+  }
 </style>
